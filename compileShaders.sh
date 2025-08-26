1f734140125bd1790651a2beea33119257eff391
Facundo <facuguialtana7@gmail.com>
2025-08-26 09:30:46 -0300

    Added proper Vulkan SDK

--- compileShaders.sh
+++ compileShaders.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./shaders/slang/build/slang-2025.14.3-linux-x86_64/bin/slangc shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o shaders/slang.spv
+./1.4.321.1/x86_64/bin/slangc shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o shaders/slang.spv
