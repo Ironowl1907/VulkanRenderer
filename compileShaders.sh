442edcc72b07821f00e526643d078d24e2c1d319
Facundo <facuguialtana7@gmail.com>
2025-08-31 10:29:59 -0300

    Working with UBO

--- compileShaders.sh
+++ compileShaders.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./shaders/slang/build/slang-2025.14.3-linux-x86_64/bin/slangc shaders/shader.slang -target spirv -profile spirv_1_4 -emit-spirv-directly -fvk-use-entrypoint-name -entry vertMain -entry fragMain -o shaders/slang.spv
+./shaders/slang/build/RelWithDebInfo/bin/slangc \
+  shaders/shader.slang \
+  -target spirv \
+  -profile spirv_1_4 \
+  -emit-spirv-directly \
+  -fvk-use-entrypoint-name \
+  -entry vertMain \
+  -entry fragMain \
+  -o shaders/slang.spv
